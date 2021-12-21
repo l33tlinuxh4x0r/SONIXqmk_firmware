99a33a7ef20d046572beacd9d6a9e02e0cf11806
Dimitris Mantzouranis <d3xter93@gmail.com>
2021-12-22 00:22:48 +0200

    rgb: minimize flickering

--- drivers/sn32/rgb_matrix_sn32f248b.c
+++ drivers/sn32/rgb_matrix_sn32f248b.c
@@ -277,12 +277,19 @@ void shared_matrix_rgb_enable(void) {
     pwmEnablePeriodicNotification(&PWMD1);
 }
 
-void shared_matrix_rgb_disable(void) {
+void shared_matrix_rgb_disable_pwm(void) {
     // Disable PWM outputs on column pins
     for(uint8_t i=0;i<24;i++){
         if (&pwmcfg.channels[i].mode != PWM_OUTPUT_DISABLED){
             pwmDisableChannel(&PWMD1,i);
         }
+    }
+}
+
+void shared_matrix_rgb_disable_leds(void) {
+    // Disable LED outputs on row pins
+    for (uint8_t x = 0; x < LED_MATRIX_ROWS_HW; x++) {
+        writePinLow(led_row_pins[x]);
     }
 }
 
@@ -292,24 +299,18 @@ void update_pwm_channels(PWMDriver *pwmp, uint8_t last_row) {
             #if(DIODE_DIRECTION == ROW2COL)
                 // Scan the key matrix
                 pwmDisableChannelI(pwmp,chan_order[i]);
-                writePinLow(led_row_pins[last_row]);
-                writePinLow(led_row_pins[last_row - 1]);
-                writePinLow(led_row_pins[last_row - 2]);
                 matrix_scan_keys(raw_matrix,chan_order[i]);
             #endif
             uint8_t led_index = g_led_config.matrix_co[row_idx][chan_order[i]];
             switch(current_row % LED_MATRIX_ROW_CHANNELS) {
             case 0:
                     pwmEnableChannelI(pwmp,i,led_state[led_index].b);
-                    writePinHigh(led_row_pins[current_row]);
                 break;
             case 1:
                     pwmEnableChannelI(pwmp,i,led_state[led_index].g);
-                    writePinHigh(led_row_pins[current_row]);
                 break;
             case 2:
                     pwmEnableChannelI(pwmp,i,led_state[led_index].r);
-                    writePinHigh(led_row_pins[current_row]);
                 break;
             default:
                 ;
@@ -330,13 +331,16 @@ void rgb_callback(PWMDriver *pwmp) {
     chSysLockFromISR();
     #if(DIODE_DIRECTION == COL2ROW)
         // Scan the key matrix
-        shared_matrix_rgb_disable();
+        shared_matrix_rgb_disable_pwm();
         writePinLow(led_row_pins[last_row]);
         writePinLow(led_row_pins[last_row - 1]);
         writePinLow(led_row_pins[last_row - 2]);
-        matrix_scan_keys(raw_matrix,row_idx);
+        matrix_scan_keys(raw_matrix, row_idx);
+    #elif(DIODE_DIRECTION == ROW2COL)
+        shared_matrix_rgb_disable_leds();
     #endif
     update_pwm_channels(pwmp, last_row);
+    writePinHigh(led_row_pins[current_row]);
     chSysUnlockFromISR();
     // Advance the timer to just before the wrap-around, that will start a new PWM cycle
     pwm_lld_change_counter(pwmp, 0xFFFF);
