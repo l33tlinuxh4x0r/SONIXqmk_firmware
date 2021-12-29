ebc8554087f739228835d3e734e60213fecb17ae
Dimitris Mantzouranis <d3xter93@gmail.com>
2021-12-29 12:14:37 +0200

    speed up building

--- bin/build_all.py
+++ bin/build_all.py
@@ -3,44 +3,11 @@ import os
 import sys
 
 BOARDS = [
-    'redragon/k552/rev1',
     'redragon/k552/rev2',
-    'redragon/k530',
     'redragon/k556',
-    'redragon/k580',
-    'redragon/k630',
-    'keychron/c1/plain',
-    'keychron/c1/rgb',
-    'keychron/c1/white',
-    'keychron/c2/white',
     'keychron/k2',
-    'keychron/k3',
     'keychron/k4/rgb/v1',
-    'keychron/k4/rgb/v2',
-    'keychron/k6/rgb',
-    'keychron/k6/rgb/via',
-    'keychron/k6/white',
-    'keychron/k7/rgb',
-    'keychron/k7/rgb/optical',
-    'keychron/k8',
-    'keychron/k8/via',
-    'keychron/k8/optical',
-    'keychron/k8/optical_via',
-    'keychron/k14/rgb',
-    'keychron/k14/rgb/via',
-    'ajazz/ak33/rev1',
-    'ajazz/ak33/rev2',
-    'smartduck/xs61',
-    'spcgear/gk530',
-    'spcgear/gk540',
-    'sharkoon/sgk3',
-    'womier/k87',
-    'flashquark/horizon_z',
-    'ffc/ffc61',
-    'gmmk/full/rev2',
-    'gmmk/full/rev3',
-    'marvo/kg938',
-    'akko/3061_rgb']
+    'keychron/k4/rgb/v2']
 
 error = False
 for kb in BOARDS:
