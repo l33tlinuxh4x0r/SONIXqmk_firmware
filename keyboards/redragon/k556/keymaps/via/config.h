9997ee906b6935494fab766dd6ef9fb21aad3b22
l33tlinuxh4x0r <l33tlinuxh4x0r@gmail.com>
2022-02-10 18:54:25 -0500

    Update config.h
    Removed lines as they are defaults as per dexter93's suggestion.

--- keyboards/redragon/k556/keymaps/via/config.h
+++ keyboards/redragon/k556/keymaps/via/config.h
@@ -18,8 +18,6 @@
 #pragma once
 
 // Define dynamic keymap layer count
-#define LAYER_STATE_8BIT
-#define DYNAMIC_KEYMAP_LAYER_COUNT 4
 #define DYNAMIC_KEYMAP_EEPROM_MAX_ADDR 1200
 
 // Enable RGB Matrix
